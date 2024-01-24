ad9a1576ef0da58339de48b9b11a6871788a878d
MohammadSatel <hamuds95@gmail.com>
2024-01-24 10:49:06 +0200

    first commit

---
+++ src/content/projects.js
@@ -0,0 +1,33 @@
+//projects start
+const projects = [
+  {
+    name: "Booking.com clone",
+    description:
+      "A clone of booking.com which is built using Nextjs 14 and Oxylabs webscraping API to scrape data from the official booking.com website and show as results of our queries",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/randilt/booking.com-clone-nextjs14",
+    img: "/bookingcom.jpg",
+    preview: "https://bookingcomnext14.netlify.app/",
+  },
+  {
+    name: "Cyberscourge Hub",
+    description:
+      "An open source ultra fast markdown blog app built using Hugo and deployed on Netlify. The theme of this app is 100% custom built by me. The theme is also open source and can be found on my github profile. And I regularly write articles on this blog.",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/randilt/cyberscourge-hub",
+    img: "/cyberscourge.jpg",
+    preview: "https://rtharusha.tech",
+  },
+  {
+    name: "Jotion (Notion Clone)",
+    description:
+      "A clone of the popular note taking app Notion. This app is built using Nextjs 13 and Tailwind CSS. It uses clerk for authentication and convex for its real time database. The project is not yet complete ",
+    stack: ["React", "NodeJS", "Express", "MongoDB"],
+    sourceCode: "https://github.com/randilt/notion-clone",
+    img: "/jotion.jpg",
+    preview: "#",
+  },
+];
+
+//projects end
+export default projects;
---
+++ src/content/skills.js
@@ -0,0 +1,19 @@
+const skills = [
+  "react.svg",
+  "nextjs.svg",
+  "js.svg",
+  "html.svg",
+  "css.svg",
+  "framer-motion.svg",
+  "nodejs.svg",
+  "express.svg",
+  "python.svg",
+  "bootstrap.svg",
+  "git.svg",
+  "github.svg",
+  "mongodb.svg",
+  "tailwind.svg",
+  "linux.svg",
+]; // Add or remove skills here, make sure to include the image respective to the skill inside the public/skills folder and use the correct file extension
+
+export default skills;
