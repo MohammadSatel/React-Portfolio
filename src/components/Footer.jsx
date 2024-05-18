d87e9c1ef940f3fff609497358220fcef2ac4d9b
MohammadSatel <hamuds95@gmail.com>
2024-05-18 19:32:11 +0300

    debug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="footer">
       <p>
         &copy; 2024 » Made with ❤️ by{" "}
-        <a href="https://github.com/MohammadSatel">Mohammad Satel ™</a>.
+        <a href="https://github.com/MohammadSatel">Mohammad Satel</a>.
       </p>
     </footer>
   );
